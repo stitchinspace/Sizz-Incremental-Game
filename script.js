a758b28e26eb74386a071b443c8ed77e29f4935a
stitchinspace <reninspace@gmail.com>
2016-06-19 14:48:01 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict"; //makes sure I'm doing it right
+document.addEventListener("DOMContentLoaded", function () {//loads page after html loads
+
+//make action of "button" class - increment #of, increase $income, increase $cost
+//make action of "menu" class show menu on hover, - change feature to purchased feature on click
+
+}); //close DOMcontentLoaded function
